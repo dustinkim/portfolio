5ac55803f1ae978190e0c9a90662bb261d8f7806
dustinkim <dustinkim@Dustins-Air.lan1>
2020-07-30 18:38:52 -0400

    create three pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `blue` }}>
+      <Link to="/about/">About</Link>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText="Dustin Kim" />
+      <p>Software Engineer | Product Manager</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  )
 }
